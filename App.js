e21576ac2892abd9cd585c4aa957625b5b79f89a
thebenoit <kemmywillbenoit@yahoo.com>
2024-02-08 01:28:29 -0500

    ajout de la composante Head

--- App.js
+++ App.js
@@ -1,9 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+function Head(){
+  return(
+    <View style={styles.container}>
+      <Text style={styles.title}>Jeu Devinette</Text>
+    </View>
+
+  )
+}
 export default function App() {
   return (
     <View style={styles.container}>
+      <Head/>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
